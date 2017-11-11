27b21ba85f688655588e9036eabdca03f08e5596
Douglas Bastos <douglashsb@gmail.com>
2017-11-11 21:06:26 -0200

    Inclui lógica para quando jogador humano estou no total de 21 pontos

--- blackjack/players/cpu_player.py
+++ blackjack/players/cpu_player.py
@@ -3,14 +3,33 @@ from blackjack.players.player import Player
 
 
 class CpuPlayer(Player):
+
+    def _opponent_played_already(self):
+        if len(self.opponent_cards) > 2:
+           return True
+        return False
+
+    def _opponent_ahead(self):
+        if self.opponent_score > self.score:
+            return True
+        return False
+
+    def _opponent_bust_card(self):
+        if self.opponent_score > 21:
+            return True
+        return False
+
+    def _is_max_limit_to_continue(self):
+        max_limit_continue = 17
+        if self.score > max_limit_continue:
+            return False
+        return True
+
     def will_continue(self):
         sleep(1)
-        if self.opponent_score:
-            if self.opponent_score > self.score:
+        if self._opponent_played_already():
+            if self._opponent_ahead() and not self._opponent_bust_card():
                 return True
             return False
         else:
-            max_limit_continue = 17
-            if self.score > max_limit_continue:
-                return False
-            return True
+            return self._is_max_limit_to_continue()
--- tests/test_cpu_player.py
+++ tests/test_cpu_player.py
@@ -5,6 +5,7 @@ from blackjack.players.cpu_player import CpuPlayer
 
 
 class CpuPlayerTest(TestCase):
+
     def test_opponent_with_the_game_finalized_and_cpu_losing(self):
         cpu_player = CpuPlayer()
         cpu_player.cards = [Card(rank='10', suit='♠'),
@@ -13,7 +14,7 @@ class CpuPlayerTest(TestCase):
                                      Card(rank='8', suit='♠'),
                                      Card(rank='2', suit='♠')]
 
-        self.assertTrue(cpu_player.will_continue(), 'Continue jogada')
+        self.assertTrue(cpu_player.will_continue(), 'Continue jogando')
 
     def test_opponent_with_the_game_finalized_and_cpu_winning(self):
         cpu_player = CpuPlayer()
@@ -25,18 +26,31 @@ class CpuPlayerTest(TestCase):
 
         self.assertFalse(cpu_player.will_continue(), 'Terminar jogada')
 
-    def test_opponent_without_having_played_stopped_when_18_points(self):
+
+    def test_opponent_bust_card(self):
         cpu_player = CpuPlayer()
-        cpu_player.cards = [Card(rank='10', suit='♠'),
-                            Card(rank='8', suit='♠')]
-        cpu_player.opponent_cards = []
+        cpu_player.cards = [Card(rank='3', suit='♠'),
+                            Card(rank='4', suit='♠')]
+        cpu_player.opponent_cards = [Card(rank='10', suit='♠'),
+                                     Card(rank='K', suit='♠'),
+                                     Card(rank='2', suit='♠')]
 
         self.assertFalse(cpu_player.will_continue(), 'Terminar jogada')
 
-    def test_opponent_without_having_played_continue_when_17_points(self):
+    def test_opponent_without_having_played_and_score_bigger_or_equal_then_18(self):
         cpu_player = CpuPlayer()
-        cpu_player.cards = [Card(rank='10', suit='♠'),
-                            Card(rank='7', suit='♠')]
-        cpu_player.opponent_cards = []
+        cpu_player.cards = [Card(rank='10', suit='♥'),
+                            Card(rank='8', suit='♥')]
+        cpu_player.opponent_cards = [Card(rank='10', suit='♠'),
+                                     Card(rank='8', suit='♠')]
 
-        self.assertTrue(cpu_player.will_continue(), 'Continue jogada')
+        self.assertFalse(cpu_player.will_continue(), 'Terminar jogada')
+
+    def test_opponent_without_having_played_and_score_less_then_17(self):
+        cpu_player = CpuPlayer()
+        cpu_player.cards = [Card(rank='10', suit='♥'),
+                            Card(rank='7', suit='♥')]
+        cpu_player.opponent_cards = [Card(rank='10', suit='♠'),
+                                     Card(rank='7', suit='♠')]
+
+        self.assertTrue(cpu_player.will_continue(), 'Continar jogadando')
